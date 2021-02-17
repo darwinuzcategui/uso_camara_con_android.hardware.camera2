157d2fd6de4b19431aa0fdd34b8dc757eb551e08
Darwin Uzcategui Portati Linux <darwin.uzcategui1973@gmail.com>
2021-02-17 19:29:42 -0400

    cambio de mensajes

--- app/src/main/java/com/example/mycamaramanager2/MainActivity.java
+++ app/src/main/java/com/example/mycamaramanager2/MainActivity.java
@@ -136,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
             super.onCaptureCompleted(session, request, result);
-            Toast.makeText(MainActivity.this, "Saved:" + file, Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Grabado:" + file, Toast.LENGTH_SHORT).show();
             createCameraPreview();
         }
     };
@@ -225,7 +225,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
                     super.onCaptureCompleted(session, request, result);
-                    Toast.makeText(MainActivity.this, "Saved:" + file, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "GRabado:" + file, Toast.LENGTH_SHORT).show();
                     createCameraPreview();
                 }
             };
@@ -270,7 +270,7 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    Toast.makeText(MainActivity.this, "Configuration change", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Configuration change*", Toast.LENGTH_SHORT).show();
                 }
             }, null);
         } catch (CameraAccessException e) {
